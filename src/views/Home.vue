034fef0c5d1e790049f36d2d53a6d3a8bc50d272
bloriot97 <bloriot97@gmail.com>
2019-10-22 08:48:23 -0400

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <ExperimentReport v-if="url" :url="url"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ExperimentReport from '@/components/ExperimentReport.vue'
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      url: null,
+    }
+  },
+  components: {
+    ExperimentReport
+  },
+  mounted: function() {
+    this.url = this.$route.query.url
+    console.log(this.url)
+  },
+}
+</script>
